eaeab9345e49854d433af2298361ee58f4bf8260
BaranovSev <aldebaran94sev@gmail.com>
2021-12-17 18:48:29 +0300

    Добавлен поиск (Search view) Строка запроса записываестя в хранилище общих настроек (SharedPreferences)

--- app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
@@ -3,16 +3,16 @@ package com.bignerdranch.android.photogallery.api
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 import retrofit2.http.Url
 
 interface FlickrApi {
-    @GET("services/rest/?method=flickr.interestingness.getList"+
-    "&api_key=33c28f30f4ad401685f973d3ca4865de"+
-            "&format=json"+
-            "&nojsoncallback=1"+
-            "&extras=url_s")
+    @GET("services/rest?method=flickr.interestingness.getList")
     fun fetchPhotos(): Call<FlickrResponse>
 
     @GET
     fun fetchUrlBytes(@Url url:String): Call<ResponseBody>
+
+    @GET("services/rest?method=flickr.photos.search")
+    fun searchPhotos(@Query ("text") query:String): Call<FlickrResponse>
 }
--- app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrResponse.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrResponse.kt
@@ -1,5 +1,5 @@
 package com.bignerdranch.android.photogallery.api
 
 class FlickrResponse {
-    lateinit var photos: PhotoResponse
+    lateinit var photos: PhotoResponse //photos: null  такое при неправильном запросе с сервера error 112
 }
