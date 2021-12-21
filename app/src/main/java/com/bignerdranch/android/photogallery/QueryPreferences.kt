b2de4d91fed61b1eefafbf9c72ec24169bc4c470
BaranovSev <aldebaran94sev@gmail.com>
2021-12-21 12:50:12 +0300

    Библиотека WorkManager. Добавлена проверка наличия новых фотографий в фоновом режиме (кнопка в панели main вкл/выкл проверку). Добавлены уведомления пользователю о новых фотографиях.

--- app/src/main/java/com/bignerdranch/android/photogallery/QueryPreferences.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/QueryPreferences.kt
@@ -5,10 +5,13 @@ import android.preference.PreferenceManager
 import androidx.core.content.edit
 
 private const val PREF_SEARCH_QUERY = "searchQuery"
+private const val PREF_LAST_RESULT_ID = "lastResultId"
+private const val PREF_IS_POLLING = "isPolling"
+
 object QueryPreferences {
     fun getStoredQuery(context: Context):String{
         val prefs = PreferenceManager.getDefaultSharedPreferences(context)
-        return prefs.getString(PREF_SEARCH_QUERY, "")!!
+        return prefs.getString(PREF_SEARCH_QUERY, "")!! // !! ненулевое утверждение (выражение перед явно не равно нуля)
     }
     fun setStoredQuery(context: Context, query: String){
         PreferenceManager.getDefaultSharedPreferences(context)
@@ -16,5 +19,23 @@ object QueryPreferences {
                 putString(PREF_SEARCH_QUERY, query)
             }
 
+    }
+    fun getLastResultId(context: Context): String{
+        return PreferenceManager.getDefaultSharedPreferences(context)
+            .getString(PREF_LAST_RESULT_ID, "")!!
+    }
+    fun setLastResultId(context: Context, lastResultId: String){
+        PreferenceManager.getDefaultSharedPreferences(context).edit {
+            putString(PREF_LAST_RESULT_ID, lastResultId)
+        }
+    }
+    fun isPolling(context: Context):Boolean{
+        return PreferenceManager.getDefaultSharedPreferences(context)
+            .getBoolean(PREF_IS_POLLING, false)
+    }
+    fun setPolling(context: Context, isOn:Boolean){
+        PreferenceManager.getDefaultSharedPreferences(context).edit {
+            putBoolean(PREF_IS_POLLING, isOn)
+        }
     }
 }
